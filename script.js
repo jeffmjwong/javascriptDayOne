a4e503cab61e21e9d9c11aefef4b82616f04fc41
Jeff Wong <jeffmjwong@gmail.com>
2018-05-14 14:01:03 +1000

    defining functions

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function fullState() {
-  console.log('Running full state!');
+function fullState(abbreviation) {
+  if (abbreviation === 'NSW') {
+    return 'New South Wales';
+  } else if (abbreviation === 'QLD') {
+    return 'Queensland';
+  } else if (abbreviation === 'VIC') {
+    return 'Victoria';
+  } else if (abbreviation === 'SA') {
+    return 'South Australia';
+  } else if (abbreviation === 'WA') {
+    return 'Western Australia';
+  }
 }
-fullState();
+
+const user_input = prompt('Enter a state abbreviation');
+const state = fullState(user_input);
+alert(state);
